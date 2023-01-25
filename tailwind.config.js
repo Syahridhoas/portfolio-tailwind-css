02cd86a24db72144308df1f7edc5e619a45461f0
syahridho <syahridhosyahputra@gmail.com>
2023-01-25 10:13:18 +0700

    portfolio tailwind css v.1.0

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "16px",
+      },
+      colors: {
+        primary: "#db2777",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
